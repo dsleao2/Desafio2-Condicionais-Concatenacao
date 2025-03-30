9fa71cd8f1f5daef0554ce5e697fc40aa26b061a
Daniele Santana <dssleao2@icloud.com>
2025-03-30 12:43:32 -0300

    Resolução desafio 2 JS

--- desafio2.js
+++ desafio2.js
@@ -1,13 +1,6 @@
-
-alert('Boas vindas ao jogo do dia secreto');
-
-let chute = prompt('Digite um numero:');
-
-
-if(chute > 0)
-{alert('O número que você escolheu é positivo');
-
-} else(chute < 0)
-{
-    alert('O número que você escolheu é negativo');
+numero = prompt('Digite um positivo ou negativo');
+if (numero > 0) {
+    alert('Número positivo!');
+} else {
+    alert('Número negativo!');
 }
