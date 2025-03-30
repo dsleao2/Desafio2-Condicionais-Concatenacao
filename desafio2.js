67b8ce15ac66ca05a87ee373309cbd260c7f33d5
Daniele Santana <dssleao2@icloud.com>
2025-03-30 12:29:03 -0300

    Desafio 2
    Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.

---
+++ desafio2.js
@@ -0,0 +1,13 @@
+
+alert('Boas vindas ao jogo do dia secreto');
+
+let chute = prompt('Digite um numero:');
+
+
+if(chute > 0)
+{alert('O número que você escolheu é positivo');
+
+} else(chute < 0)
+{
+    alert('O número que você escolheu é negativo');
+}
